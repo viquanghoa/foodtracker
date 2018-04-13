6eba4a35e722abeb589255b099aa691ac5d7d48f
Vi Quang Hoa <vyquanghoa@gmail.com>
2018-04-13 10:53:24 +0700

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Vi Quang Hoà on 4/13/18.
+//  Copyright © 2018 Vi Quang Hoà. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
